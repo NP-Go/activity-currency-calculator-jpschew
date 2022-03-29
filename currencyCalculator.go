f1567b1724c3262dd602b41d9e14327fcee8c756
jpschew <jps.chew@gmail.com>
2022-03-29 23:46:22 +0800

    added code for currency calculator function

--- currencyCalculator.go
+++ currencyCalculator.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func CurrencyCalculator(oneDollar, fiftyCent, twentyCent, tenCent, fiveCent float64) (float64, float64, float64) {
 	//Insert your code here
+	totalAmount := oneDollar*1 + fiftyCent*0.5 + twentyCent*0.2 + tenCent*0.1 + fiveCent*0.05
+	twoDollarNotes := 0.0
+	changeAmount := totalAmount
+	for changeAmount > 2 {
+		twoDollarNotes += 1
+		changeAmount -= 2
+	}
 
 	//Do not remove this
+	changeAmount = math.Round(changeAmount*100) / 100
 	fmt.Println("Total:", totalAmount, "Two Dollar Notes:", twoDollarNotes, " Change: ", changeAmount)
 	return totalAmount, twoDollarNotes, changeAmount
 }
